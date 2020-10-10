b8e42eb1de79fc1f5ec379b0975de181775e4176
fan4588 <zfcaomeij>
2020-10-10 15:08:16 +0800

    凡叔

--- src/main/java/com/nephew/jk/domain/User.java
+++ src/main/java/com/nephew/jk/domain/User.java
@@ -1,9 +1,6 @@
 package com.nephew.jk.domain;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
-import springfox.documentation.schema.Model;
 
 import java.io.Serializable;
 
@@ -16,9 +13,6 @@ import java.io.Serializable;
  * @since 2020-08-17
  */
 @Data
-@EqualsAndHashCode(callSuper = false)
-@Accessors(chain = true)
-//@KeySequence
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -30,53 +24,6 @@ public class User implements Serializable {
     private String age;
 
     private String sex;
-
-    private String image;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public static long getSerialVersionUID() {
-        return serialVersionUID;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
 
 
 }
